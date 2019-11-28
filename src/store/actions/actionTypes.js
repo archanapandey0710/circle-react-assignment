b91b0c1e8831cd2c27dbf296023aa8093d9b1518
Archana Pandey <archana.pandey@aquariansoft.in>
2019-11-28 12:11:55 +0530

    added space between the twi methods

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -1,3 +1,4 @@
 //Employee Action Types
+
 export const GET_EMPLOYEES_LIST = "GET_EMPLOYEES_LIST";
 export const LOGIN = "LOGIN";
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -3,6 +3,7 @@ import { combineReducers } from "redux";
 //import Reducers
 import employeeReducer from "./employeeReducer";
 import loginReducer from "./loginReducer";
+
 const reducers = combineReducers({ employeeReducer, loginReducer });
 
 export default reducers;
