c99ac43cb91084fce50d1a59724b952c1ae14ef8
Archana Pandey <archana.pandey@aquariansoft.in>
2019-11-28 17:33:37 +0530

    Updates

---
+++ precache-manifest.b602828c5b6192f37102d9471b0b86cf.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d1f257af547c5f7a18c697071c08861",
+    "url": "/circle-react-assignment/index.html"
+  },
+  {
+    "revision": "ba6df4aee27ddedc2db0",
+    "url": "/circle-react-assignment/static/css/2.23dd8e4f.chunk.css"
+  },
+  {
+    "revision": "5facd0ba98e9a469d151",
+    "url": "/circle-react-assignment/static/css/main.6ad5e6e2.chunk.css"
+  },
+  {
+    "revision": "ba6df4aee27ddedc2db0",
+    "url": "/circle-react-assignment/static/js/2.f14fed66.chunk.js"
+  },
+  {
+    "revision": "5facd0ba98e9a469d151",
+    "url": "/circle-react-assignment/static/js/main.d3573a1c.chunk.js"
+  },
+  {
+    "revision": "cb4d672a3e0ff130889b",
+    "url": "/circle-react-assignment/static/js/runtime-main.f8cd759d.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/circle-react-assignment/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/circle-react-assignment/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/circle-react-assignment/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/circle-react-assignment/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/circle-react-assignment/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "c513565627ab6539824e0744827c51a2",
+    "url": "/circle-react-assignment/static/media/logo.c5135656.png"
+  }
+]);
